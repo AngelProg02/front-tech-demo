09900de4ad0010a8fe444224dc250e7d3cfb4d77
angel_luis_vn <angelluisvn@gmail.com>
2023-09-03 23:54:11 +0200

    Update -- "Search bar new option"

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -94,7 +94,7 @@ export const HomeScreen = () => {
                 <SearchIcon />
               </SearchIconWrapper>
               <input
-                id="prueba"
+                id="input-search"
                 type="text"
                 placeholder="Buscar"
                 onChange={handleChange}
@@ -137,30 +137,38 @@ export const HomeScreen = () => {
                   />
                 </ImageListItem>
               ))
-            : beerList.map((item, index) => (
-                <ImageListItem key={index}>
-                  <img
-                    src={`${item.imageUrl}?w=248&fit=crop&auto=format`}
-                    srcSet={`${item.imageUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                    alt={item.name}
-                    loading="lazy"
-                  />
-                  <ImageListItemBar
-                    title={item.name}
-                    subtitle={item.tagline}
-                    actionIcon={
-                      <Link to={`/beer/${item.id}`}>
-                        <IconButton
-                          sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                          aria-label={`info about ${item.name}`}
-                        >
-                          <InfoIcon />
-                        </IconButton>
-                      </Link>
-                    }
-                  />
-                </ImageListItem>
-              ))}
+            : beerWanted && (
+                <p id="errorMesage">
+                  No se ha encontrado ninguna cerveza con los datos
+                  introducidos.
+                </p>
+              )}
+
+          {!beerWanted &&
+            beerList.map((item, index) => (
+              <ImageListItem key={index}>
+                <img
+                  src={`${item.imageUrl}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.imageUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.name}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.name}
+                  subtitle={item.tagline}
+                  actionIcon={
+                    <Link to={`/beer/${item.id}`}>
+                      <IconButton
+                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                        aria-label={`info about ${item.name}`}
+                      >
+                        <InfoIcon />
+                      </IconButton>
+                    </Link>
+                  }
+                />
+              </ImageListItem>
+            ))}
         </ImageList>
       </Container>
     </section>
